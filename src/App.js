7ff13ea1f69082912158dfd256449cb85092e82d
jedhabush <jedhabush@gmail.com>
2023-01-08 16:29:14 +1100

    Data section complete

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 
 import Contacts from "./scenes/contacts";
-/* import Invoices from "./scenes/invoices"
-import Bar from "./scenes/bar"
+import Invoices from "./scenes/invoices";
+/* import Bar from "./scenes/bar"
 import Form from "./scenes/form"
 import Line from "./scenes/line"
 import Pie from "./scenes/pie"
@@ -31,8 +31,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path ="/invoices" element = {<Invoices />} />
-              <Route path ="/form " element = {<Form />} />
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path ="/form " element = {<Form />} />
               <Route path ="/bar" element = {<Bar />} />
               <Route path ="/pie" element = {<Pie />} />
               <Route path ="/line" element = {<Line />} />
